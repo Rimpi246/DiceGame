af09a97bf094e5318642c746c758a913bf5a0d57
Particle <73873611+Rimpi246@users.noreply.github.com>
2020-11-28 16:32:42 +0530

    Add initial DiceGame files

---
+++ index.js
@@ -0,0 +1,21 @@
+
+var randomnumber1=Math.floor(Math.random()*6+1);
+document.querySelector("h1").setAttribute("src","images/dice" + randomnumber1+ ".png");
+
+undefined
+var dicenumber1=Math.random()*6+1;
+dicenumber1=Math.floor(dicenumber1);
+document.querySelector("img.img1").src=("images/dice" + dicenumber1 + ".png");
+
+var dicenumber2=Math.random()*6+1;
+dicenumber2=Math.floor(dicenumber2);
+document.querySelector("img.img2").src=("images/dice" + dicenumber2 + ".png");
+
+if(dicenumber1>dicenumber2){
+document.querySelector("h1").innerHTML="🚩 Player 1 wins!";}
+
+if(dicenumber1<dicenumber2){
+document.querySelector("h1").innerHTML="Player 2 wins! 🚩";}
+
+if(dicenumber1===dicenumber2){
+document.querySelector("h1").innerHTML="🎊 Draw! 🎊";}
