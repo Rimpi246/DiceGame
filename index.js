5177b158b8a120f8f03e62d82ad633f3cd3edb4a
Particle <73873611+Rimpi246@users.noreply.github.com>
2020-11-28 16:42:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 
 var randomnumber1=Math.floor(Math.random()*6+1);
-document.querySelector("h1").setAttribute("src","images/dice" + randomnumber1+ ".png");
+document.querySelector("h1").setAttribute("src","dice" + randomnumber1+ ".png");
 
 undefined
 var dicenumber1=Math.random()*6+1;
 dicenumber1=Math.floor(dicenumber1);
-document.querySelector("img.img1").src=("images/dice" + dicenumber1 + ".png");
+document.querySelector("img.img1").src=("dice" + dicenumber1 + ".png");
 
 var dicenumber2=Math.random()*6+1;
 dicenumber2=Math.floor(dicenumber2);
-document.querySelector("img.img2").src=("images/dice" + dicenumber2 + ".png");
+document.querySelector("img.img2").src=("dice" + dicenumber2 + ".png");
 
 if(dicenumber1>dicenumber2){
 document.querySelector("h1").innerHTML="🚩 Player 1 wins!";}
